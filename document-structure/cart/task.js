1afed7a0d2a95f7e22a67521644d8d3478138c95
Vasilij Golnev <vasilij.golnev@yandex.ru>
2022-02-20 18:54:12 +0700

    Cart

---
+++ document-structure/cart/task.js
@@ -0,0 +1,63 @@
+const productDec = document.querySelectorAll(".product__quantity-control_dec");
+const productInc = document.querySelectorAll(".product__quantity-control_inc");
+const productAdd = document.querySelectorAll(".product__add");
+const cartProducts = document.querySelector(".cart__products");
+let sumDec = 0;
+let sumInc = 0;
+
+function decrease(event) {
+    sumDec = Number(event.target.nextElementSibling.innerText);
+    if (sumDec > 0) {
+        sumDec -= 1;
+        event.target.nextElementSibling.innerText = sumDec;
+        console.log(event.target.nextElementSibling.innerText);
+    }
+}
+
+function increase(event) {
+    if (sumInc === 0 || sumInc > 0) {
+        let sumInc = Number(event.target.previousElementSibling.innerText);
+        sumInc += 1;
+        event.target.previousElementSibling.innerText = sumInc;
+    }
+}
+
+function addBusket(event) {
+    let id = event.target.closest(".product").dataset.id;
+    let imgSrc = event.target.closest(".product__controls").previousElementSibling.src;
+    let numberValue = event.target.previousElementSibling.querySelector(".product__quantity-value").innerText;
+    let div = document.createElement("div");
+    let addElem = cartProducts.appendChild(div);
+
+    const cartProduct = Array.from(document.querySelectorAll(".cart__product"));
+
+    if (cartProduct.length === 0 && cartProduct.length < 1) {
+        addElem.outerHTML = `<div class="cart__product" data-id=${id}><img class="cart__product-image" src=${imgSrc}><div class="cart__product-count">${numberValue}</div></div>`;
+    }
+
+    if (cartProduct.length > 0) {
+        for (let item of cartProduct) {
+            if (id === item.dataset.id) {
+                addElem.remove();
+                item.outerHTML = `<div class="cart__product" data-id=${id}><img class="cart__product-image" src=${imgSrc}><div class="cart__product-count">${numberValue}</div></div>`;
+            }
+        }
+
+        let mapId = cartProduct.map((item) => item.dataset.id);
+        if (!mapId.includes(id)) {
+            addElem.outerHTML = `<div class="cart__product" data-id=${id}><img class="cart__product-image" src=${imgSrc}><div class="cart__product-count">${numberValue}</div></div>`;
+        }
+    }
+}
+
+for (let item of productDec) {
+    item.addEventListener("click", decrease);
+}
+
+for (let item of productInc) {
+    item.addEventListener("click", increase);
+}
+
+for (let item of productAdd) {
+    item.addEventListener("click", addBusket);
+}
