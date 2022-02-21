2890d9cb86f4d5894becaf22269cf1288012fb41
Vasilij Golnev <vasilij.golnev@yandex.ru>
2022-02-21 18:08:25 +0700

    Todo2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,21 +3,20 @@ const taskInput = document.getElementById("task__input");
 
 
 window.addEventListener("keydown", (event) => {
-   if (event.keyCode === 13 && taskInput.value.trim().length > 0) {
-      event.preventDefault();
+   if (event.key === "Enter" && taskInput.value.trim().length > 0) {
+
       let divv = tasksList.appendChild(document.createElement("div"));
       divv.outerHTML = `<div class="task"><div class="task__title">${taskInput.value}</div><a href="#" class="task__remove">&times;</a></div>`;
       taskInput.value = null;
 
-      let removeTask = document.querySelectorAll(".task");
 
-      for (let item of removeTask) {
-         item.lastElementChild.addEventListener("click", (event) => { event.target.closest(".task").remove() });
+      removeTask = document.querySelectorAll(".task__remove");
+
+      for (let i = 0; i < removeTask.length; i++) {
+         let removeTaskLength = removeTask.length - 1;
+         removeTask[removeTaskLength].addEventListener("click", (event) => { event.target.closest(".task").remove() });
       }
    }
-
-   if (event.keyCode === 13 && taskInput.value.trim().length === 0) {
-      event.preventDefault();
-   }
-
 });
+
+taskInput.addEventListener("submit", () => { event.preventDefault(); });
