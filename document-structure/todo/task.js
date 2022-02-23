9c1f8b889d029d1e3988148a97853ed609302a0f
Vasilij Golnev <vasilij.golnev@yandex.ru>
2022-02-23 16:22:48 +0700

    Todo3

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,22 +1,22 @@
 const tasksList = document.getElementById("tasks__list");
 const taskInput = document.getElementById("task__input");
+let tasksAdd = document.getElementById("tasks__add");
 
+function addForm(event) {
 
-window.addEventListener("keydown", (event) => {
-   if (event.key === "Enter" && taskInput.value.trim().length > 0) {
+   if (event.key === "Enter" || event.target === tasksAdd && taskInput.value.trim().length > 0) {
 
       let divv = tasksList.appendChild(document.createElement("div"));
       divv.outerHTML = `<div class="task"><div class="task__title">${taskInput.value}</div><a href="#" class="task__remove">&times;</a></div>`;
       taskInput.value = null;
 
-
       removeTask = document.querySelectorAll(".task__remove");
-
-      for (let i = 0; i < removeTask.length; i++) {
-         let removeTaskLength = removeTask.length - 1;
-         removeTask[removeTaskLength].addEventListener("click", (event) => { event.target.closest(".task").remove() });
-      }
+      let removeTaskLength = removeTask.length - 1;
+      removeTask[removeTaskLength].addEventListener("click", (event) => { event.target.closest(".task").remove() });
    }
-});
+}
 
-taskInput.addEventListener("submit", () => { event.preventDefault(); });
+
+window.addEventListener("keydown", addForm);
+tasksAdd.addEventListener("click", addForm);
+document.getElementById("tasks__form").addEventListener("submit", () => { event.preventDefault(); });
